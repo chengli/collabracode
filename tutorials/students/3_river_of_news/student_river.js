aa0c16a2fd9359d1f23d13a53b0164fe43536dc0
Cheng-Lun Li <li.chenglun@gmail.com>
2011-05-07 11:05:33 -0400

    Updated jQuery version of river of news

--- tutorials/students/3_river_of_news/student_river.js
+++ tutorials/students/3_river_of_news/student_river.js
@@ -1,39 +1,23 @@
 var riverCallback = {
   "onsuccess" : function(obj) {
     // TODO
-    log("SUCCESS: Loaded rss.json file");
-    var title = document.getElementById("title");
-    title.textContent = obj.rss.channel.title;
-    var river = document.getElementById("river");
-    //alert(obj.rss.channel.item[0]);
-    //var item = obj.rss.channel.item;
-    //var news = item[0];
+    var body = $('<div></div>')
+    $.getJSON("http://collabracode-js.appspot.com/rss.json",function(data){
+    	var newsItem = data.rss.channel.item;
+   		$.each(newsItem, function(key, val){
+   			var news = $('<div></div>');
+   			news.html('<br />'+'<b>'+val.title+'</b>' + '<br /> Link: ' + '<a>' + val.link + '</a>' + '<br /> Description: '+ val.description + '<br />');
+   			body.append(news);
+   						
+   		});
+    });
+	console.log(body);
 
-	/*
-    var news_item = document.createElement("div");
-    var subtitle = document.createElement("p"); 
-    subtitle.appendChild(document.createTextNode(news["title"]));
-	news_item.appendChild(subtitle);
-	
-	river.appendChild(news_item);
-	*/	
-    var river_of_news = obj.rss.channel.item;
-    for(items in river_of_news){
-    	alert(items["title"]);
-    	//var news_item = document.createElement("div");
-   
-    	//var subtitle = document.createElement("p"); 
-    	//subtitle.appendChild(document.createTextNode(items["title"]));
-		//news_item.appendChild(subtitle);
-		
-		//river.appendChild(news_item);
-		
-		    	
-    }			
-    
-    
-    //console.log(obj);
-    
+    $(function(){
+    	$("#river").append(body);
+    })
+    console.log(body);
+    log("SUCCESS: Loaded rss.json file");
   },
   "onerror" : function(req) {
     log("ERROR: Unable to download rss.json file" + req);
